f1c599092d6479e093b16252367e9f71514cd25f
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-11-28 12:45:05 +0100

    commit

--- AreaCubo.py
+++ AreaCubo.py
@@ -1,1 +1,8 @@
-#diobuono
+#acquisisci in input la lunghezza del lato di un cubo(sotto forma di intero) e visualizza l'area della sua superficie
+
+side = int(input("Insert side of cube: "))
+
+Aquadrato = side*side
+area = 6 * Aquadrato
+
+print("Area cube is " + str(area) + " cm2")
