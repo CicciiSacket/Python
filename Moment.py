bffac5a1fe67e2689026d5b9fc2a48c37a4e8fcf
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-11-28 14:25:31 +0100

    commit

---
+++ Moment.py
@@ -0,0 +1,7 @@
+#momento di un oggetto = massa * velocità
+
+Mass = float(input("Insert mass of object : "))
+Speed = float(input("Insert speed of object : "))
+Moment = float(Mass * Speed)
+
+print("Moment is: " + str(Moment) + "kg m/s")
