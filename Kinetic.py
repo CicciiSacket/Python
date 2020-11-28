b6348fb6b5e4859f271d42ca6cb2a79da749a265
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-11-28 14:30:06 +0100

    commit

---
+++ Kinetic.py
@@ -0,0 +1,8 @@
+#energia cinetica di un oggetto
+
+Mass = float(input("Insert mass of object : "))
+Speed = float(input("Insert speed of object : "))
+KE = 0.5 * Mass * pow(Speed,2)
+
+print("KE is: " + str(KE)+"J")
+
