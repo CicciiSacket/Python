cd67a41c18158efc3e14deebdf1cd62ed1c7eb7c
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2020-12-09 10:48:36 +0100

    commit

--- ExercitesMix.py
+++ ExercitesMix.py
@@ -11,4 +11,18 @@ import math
 N = 8
 print(float(math.log(N,2))) #definito con metodi di math
 
+#approssimazione della radice quadrata
+import math 
 
+x = float(input("Enter a positive number: "))
+tolerance = 0.000001
+estimate = 1
+
+while True:
+  estimate = (estimate + x / estimate) / 2
+  difference = abs(x - estimate ** x)
+  if difference <= tolerance:
+    break
+print("the program estimate: " , estimate)
+print("the python's estimate: " , math.sqrt(x))      
+      
