46abc7b682babd8361e72966112efa14dd003e7c
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-12 13:01:36 +0100

    Analysis

--- Data Analysis/FirstLesson.py
+++ Data Analysis/FirstLesson.py
@@ -8,3 +8,5 @@ print(c)
 
 d = "pippo"
 print(d*3) #stampa 3 volte la variabile d, quindi + e * con le stringhe operano in maniera diversa, OCCHIO AL CASTING!
+
+#INDENTAZIONE!!!! 4 spazi sono un nuovo blocco[iterazione]
