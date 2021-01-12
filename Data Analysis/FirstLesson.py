af35923d2430f4dec290b372ec8c6647def2ca45
Studente <studente@MBP-di-Admin.homenet.telecomitalia.it>
2021-01-12 12:10:12 +0100

    Analysis

---
+++ Data Analysis/FirstLesson.py
@@ -0,0 +1,10 @@
+"""
+CAPITOLO 2, prima lezione academy
+"""
+a = 1
+b = 2 
+c = a + b #i tipi di dato in python sono lazy ovvero interpreta a seconda di cosa si scrive, OCCHIO AL CASTING!
+print(c) 
+
+d = "pippo"
+print(d*3) #stampa 3 volte la variabile d, quindi + e * con le stringhe operano in maniera diversa, OCCHIO AL CASTING!
