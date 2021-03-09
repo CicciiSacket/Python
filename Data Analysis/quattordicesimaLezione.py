abe912757fefd3799d47fa38852af79a4e4e5a20
CicciiSacket <55915430+CicciiSacket@users.noreply.github.com>
2021-03-09 11:39:31 +0100

    quattordicesimaLezione

--- Data Analysis/quattordicesimaLezione.py
+++ Data Analysis/quattordicesimaLezione.py
@@ -36,13 +36,37 @@ P["zTot"] = (P["popTot"] - P["popTot"].mean()) /  P["popTot"].std() #aggiunta di
 P["zCom"] = (P["comuni"] - P["comuni"].mean()) /  P["comuni"].std() 
 P.to_csv("Data Analysis/Province.csv")
 
-
 plt.figure("TOT vs COM")
 plt.plot(P["zTot"],P["zCom"],"or",markerSize=10)
-plt.show()
+
+P.corr()
+plt.figure('mappa di calore, dal più caldo nero al più freddo bianco')
+plt.imshow(P.corr(),cmap='hot', interpolation='nearest')
+
+plt.figure('pop vs sup')
+plt.plot(P["zTot"],P["sup"],"or",markerSize=10)
+
+plt.figure('com vs sup')
+plt.plot(P["comuni"],P["sup"],"or",markerSize=10)
 
 
+#nuovo dataframe a partire dal precedente, rimuovendo l'outlier, in questo caso messina
+P2 = pd.DataFrame(P["maschi"])
+P2["femmine"] = P["femmine"]
+P2["sup"] = P["sup"]
+P2 ["comuni"] = P["comuni"]
+P2 = P2.drop("ME")
+plt.figure('com vs sup senza messina')
+plt.plot(P2["comuni"],P2["sup"],"or",markerSize=10)
+
+P2.corr()
+plt.figure('mappa di calore, dal più caldo nero al più freddo bianco 2')
+plt.imshow(P2.corr(),cmap='hot', interpolation='nearest')
+
+# plt.show()
 """ 
 Covarianza(x,y)= 1/N ∑(xi - mx) (yi - my)   
 Z_scoring = (voto - media) / (deviazione standard dei voti) #tutto viene riportato nella stessa scala a differenza dei dati in forma normale
+Regressione = costruire un modello a partire dai dati [ Abuso di linguaggio storico, in quanto non vi è nessuna regressione ma semplicemente un modello matematico ]
 """
+
