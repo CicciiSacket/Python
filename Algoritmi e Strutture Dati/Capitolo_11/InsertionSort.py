6c4eaf2f8b0ebdefeb3529c8cd89e2fc04af3278
Francesco Sacco <cicciisacco@gmail.com>
2022-03-17 10:21:06 +0100

    InsertionSort with py's book

---
+++ Algoritmi e Strutture Dati/Capitolo_11/InsertionSort.py
@@ -0,0 +1,18 @@
+def InsertionSort(A):
+    for index in range(len(A)):
+        current = A[index] #Primo elemento della lisata
+        i = index - 1 #Variabile i, ovvero elemento precedente a quello corrente
+        while i >= 0 and A[i] > current: #Cicla finchè vi sono elementi che in posizione i sono maggiori dell'elemento corrente
+            A[i + 1] = A[i] #L'elemento più grande del corrente viene spostato di uno verso destra
+            i -= 1 #Decremento di i per studiare l'elemento precedente
+        A[i + 1] = current  #Si assegna alla posizione corretta l'elemento corrente
+
+def main():
+    A = [5,0,12,43,1,6,99]
+    print(A) #Stampa lista non ordinata
+    InsertionSort(A)
+    print(A) #Stampa lista ordinata
+
+
+if __name__ == "__main__":
+    main()
