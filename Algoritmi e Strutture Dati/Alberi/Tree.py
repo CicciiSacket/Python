00f9a5f53875b889c2fcb38a93e3a9790fc2b8bb
Francesco Sacco <cicciisacco@gmail.com>
2022-04-13 21:16:38 +0200

    v1.1 add_node in Tree_Binary

--- Algoritmi e Strutture Dati/Alberi/Tree.py
+++ Algoritmi e Strutture Dati/Alberi/Tree.py
@@ -1,4 +1,3 @@
-from re import X
 from Nodo import nodo
 
 class Tree(object):
@@ -7,39 +6,36 @@ class Tree(object):
         self.tree = {}
 
     def view_tree(self):
-        i = 0
-        for i in self.tree.keys():
-            print('\n',self.tree[i],'\n')
+        print("Node info for tree are:")
+        print(self.tree)
+        return self.tree
+
+    def view_tree_info(self):
+        print("All node in tree are:")
+        for _ in self.tree.keys():
+            print('\n',self.tree[_],'\n')
         return self.tree
 
     def add_node_root(self,key):
         label = 'root'
         node = nodo(key,None,None,None,label)
-        self.tree[label] = { 'key':node.key, 'child_right':node.child_right, 'child_left':node.child_left,'parent':node.parent_node }
+        self.tree[label] = { 'label': label,'key': node.key, 'child_right': node.child_right, 'child_left': node.child_left,'parent': node.parent_node }
         return self.tree
 
     def add_node(self,new_node_key,new_node_label,parent):
+        if not parent in self.tree.keys():
+            return Exception('Parent does not exist!')
         z = nodo(new_node_key,None,None,parent,new_node_label)
-        self.tree[new_node_label] = { 'key':z.key, 'child_right':z.child_right, 'child_left':z.child_left,'parent':z.parent_node }
-        if self.tree[parent]['key'] >= z.key:
-            self.tree[parent]['child_left'] = { 'label':new_node_label, 'key':z.key, 'child_right':z.child_right, 'child_left':z.child_left,'parent':z.parent_node }
-            return 201
-        if self.tree[parent]['key'] <= z.key:
-            self.tree[parent]['child_right'] = { 'label':new_node_label, 'key':z.key, 'child_right':z.child_right, 'child_left':z.child_left,'parent':z.parent_node }
-            return 201
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        self.tree[new_node_label] = { 'label': z.label,'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
+        if self.tree[parent]['key'] > z.key and self.tree[parent]['child_left'] is None:
+            self.tree[parent]['child_left'] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
+            if self.tree[parent]['child_left'] is not None:
+                #Si sceglie come padre quello precedente
+                pass 
+        if self.tree[parent]['key'] < z.key and self.tree[parent]['child_right'] is None:
+            self.tree[parent]['child_right'] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
+            if self.tree[parent]['child_left'] is not None:
+                #Si sceglie come padre quello precedente
+                pass
+        else:
+            return Exception('A child already exists for this node!')
--- Algoritmi e Strutture Dati/Alberi/main.py
+++ Algoritmi e Strutture Dati/Alberi/main.py
@@ -1,15 +1,26 @@
-from traceback import print_list
 from Tree import Tree
 
 def main():
     tree = Tree()
     tree.add_node_root(96)
-    tree.add_node(6,'Figlio-1','root')
-    tree.add_node(1,'Figlio-2','Figlio-1')
-    print('#########')
-    print(tree.view_tree())
-    print('#########')
 
+    tree.add_node(6,'Figlio-1','root')
+    tree.add_node(100,'Figlio-2','root')
+    
+    tree.add_node(88,'Figlio-3','root')
+
+
+    # tree.add_node(1,'Figlio-2','Figlio-1')
+    # tree.add_node(2,'Figlio-3','Figlio-1')
+
+    
+    # print(tree.add_node(1,'Figlio-2','Figlio-1'))
+    # print(tree.add_node(1,'Figlio-8','Figlio-7'))
+
+    print('\n#########')
+    # tree.view_tree()
+    print('#########\n')
+    # tree.view_tree_info()
 
 if __name__ == "__main__":
     main()
