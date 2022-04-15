6deac8b6ab4396d64e5edae21cd115798c07cf5c
Francesco Sacco <cicciisacco@gmail.com>
2022-04-15 13:37:09 +0200

    Fix and upgrade

--- Algoritmi e Strutture Dati/Alberi/Tree.py
+++ Algoritmi e Strutture Dati/Alberi/Tree.py
@@ -1,3 +1,5 @@
+from errno import ESTALE
+from tkinter.messagebox import NO
 from Nodo import nodo
 
 class Tree(object):
@@ -6,6 +8,7 @@ class Tree(object):
         self.tree = {}
 
     def view_tree(self):
+        """ Info dell'albero """
         print("Node info for tree are:")
         for _ in self.tree.keys():
             tmp = nodo(self.tree[_]['key'],self.tree[_]['child_left'],self.tree[_]['child_right'],self.tree[_]['parent'],self.tree[_]['label'])
@@ -13,18 +16,21 @@ class Tree(object):
         return self.tree
 
     def view_tree_info(self):
+        """ Info sui singoli nodi dell'albero """
         print("All node in tree are:")
         for _ in self.tree.keys():
             print('\n','node:',self.tree[_]['label'],'-- key:',self.tree[_]['key'],'\n')
         return self.tree
 
     def add_node_root(self,key):
+        """ Aggiunta del nodo radice nell'oggetto albero """
         label = 'root'
         node = nodo(key,None,None,None,label)
         self.tree[label] = { 'label': label,'key': node.key, 'child_right': node.child_right, 'child_left': node.child_left,'parent': node.parent_node }
         return self.tree
 
     def add_node(self,new_node_key,new_node_label):
+        """ Aggiunta di un nodo nell'albero, se vuoto l'albero inserisce il nodo come radice """
         z = nodo(new_node_key,None,None,None,new_node_label)
         if z.key is None or z.label is None:
             return Exception('Chiave o label errati!')
@@ -40,12 +46,72 @@ class Tree(object):
         if y == None:
             self.tree['root'] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
         elif z.key < y['key']:
+            """  self.tree[z.label] per avere la stampa di tutti i nodi separati; Vedi metodo view_tree() """
             self.tree[z.label] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
             y['child_left'] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': y['label'] }
         else:
+            """  self.tree[z.label] per avere la stampa di tutti i nodi separati; Vedi metodo view_tree() """
             self.tree[z.label] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': z.parent_node }
             y['child_right'] = { 'label': z.label, 'key': z.key, 'child_right': z.child_right, 'child_left': z.child_left,'parent': y['label'] }
         return self.tree
+
+    def search_node(self,search_key):
+        """ Ricerca di un nodo mediante key; Se non presente torna nodo radice"""
+        x = self.tree['root']
+        if not x:
+            return Exception('nodo root vuoto!')
+        if search_key is None:
+            return Exception('Chiave di ricerca vuota non ammissibile!')
+        while x != None and search_key != x['key']:
+            if search_key < x['key']:
+                x = x['child_left']
+            else:
+                x = x['child_right']
+        return x
+
+    def tree_min(self):
+        """ Ricerca del valore minimo dell'albero """
+        x = self.tree['root']
+        if not x:
+            return Exception('nodo root vuoto!')
+        while x['child_left'] != None:
+            x = x['child_left']
+        return x
+
+    def tree_max(self):
+        """ Ricerca del valore minimo dell'albero """
+        x = self.tree['root']
+        if not x:
+            return Exception('nodo root vuoto!')
+        while x['child_right'] != None:
+            x = x['child_right']
+        return x
+
+    def tree_successor(self,node):
+        """ Successore di un nodo 'node' è il nodo con la chiave più piccola ma maggiore di node['key']; -> None se la chiave del nodo è la maggiore nell'albero """
+        if not node:
+            return Exception('Il nodo parametro non esiste!')
+        if node['child_right'] is None:
+            return self.tree_min()
+        y = node['parent']
+        while y != None and node == y['child_right']:
+            node = y
+            y = y['parent']
+        return y
+
+    def tree_predecessor(self,node):
+        """ Predecessore di un nodo 'node' ... """
+        if not node:
+            return Exception('Il nodo parametro non esiste!')
+        if node['child_left'] is None:
+            return self.tree_max()
+        y = node['parent']
+        while y != None and node == y['child_left']:
+            node = y
+            y = y['parent']
+        return y
+
+
 
 
 
--- Algoritmi e Strutture Dati/Alberi/main.py
+++ Algoritmi e Strutture Dati/Alberi/main.py
@@ -7,26 +7,24 @@ def main():
     tree.add_node(88,'Figlio-3')
     tree.add_node(6,'Figlio-1')
     tree.add_node(100,'Figlio-2')
+    print('\n########\n')
+    # print(tree.add_node(None,'Figlio-2'))
+    # print(tree.add_node(111,None))
+    # print('\n########\n')
+    # print(tree.search_node(6))
+    # print('\n########\n')
+    # print(tree.tree_min())
+    # print('\n########\n')
+    # print(tree.tree_max())
+    # print('\n########\n')
+    print(tree.tree_successor(tree.search_node(88)))
+    print(tree.tree_predecessor(tree.search_node(6)))
 
+    print('\n#### END ####\n')
 
-    print(tree.add_node(None,'Figlio-2'))
-    print(tree.add_node(111,None))
-
-
-
-
-    # tree.add_node(1,'Figlio-2','Figlio-1')
-    # tree.add_node(2,'Figlio-3','Figlio-1')
-
-    
-    # print(tree.add_node(1,'Figlio-2','Figlio-1'))
-    # print(tree.add_node(1,'Figlio-8','Figlio-7'))
-
-    print('\n#########')
-    print('#########\n')
     # tree.view_tree_info()
-    tree.view_tree()
+    # tree.view_tree()
 
-    print('#########\n')
+
 if __name__ == "__main__":
     main()
